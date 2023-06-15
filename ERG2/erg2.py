1f4e0e0e25945326b738b222cb590ba1f721c82e
david011011 <parathyrasdavid@gmail.com>
2023-06-15 17:46:28 +0300

    update

--- ERG2/erg2.py
+++ ERG2/erg2.py
@@ -1,36 +1,33 @@
-import numpy as np
-import pyswarms as ps
+import numpy as np, pyswarms as ps
 
 def evaluate_fitness(positions, num_items, max_weight, items):
-    total_value = np.dot(positions, items[:, 0])
-    total_weight = np.dot(positions, items[:, 1])
+    total_value = np.dot(positions, items[:, 0]) # Dot product of positions and items value (first column)
+    total_weight = np.dot(positions, items[:, 1]) # Dot product of positions and items weight (second column)
 
-    if np.any(total_weight > max_weight):
+    if np.any(total_weight > max_weight): # If any of the total weights are greater than the maximum weight
         return 0
-    else:
+    else: # Else
         return total_value
 
 def optimize_knapsack(num_items, max_weight, items, num_particles, max_iterations):
-    bounds = (np.zeros(num_items), np.ones(num_items))
+    bounds = (np.zeros(num_items), np.ones(num_items)) # Bounds for each item (0 or 1)
 
     def objective_function(positions):
-        return -evaluate_fitness(positions, num_items, max_weight, items)
+        return -evaluate_fitness(positions, num_items, max_weight, items) # Return negative fitness
 
-    options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}  # Set your preferred options here
-
-    optimizer = ps.single.GlobalBestPSO(n_particles=num_particles, dimensions=num_items, options=options, bounds=bounds)
-    best_position, best_fitness = optimizer.optimize(objective_function, iters=max_iterations)
+    options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9} # Opions for optimizer
+    optimizer = ps.single.GlobalBestPSO(n_particles=num_particles, dimensions=num_items, options=options, bounds=bounds) # Initialize optimizer
+    best_position, best_fitness = optimizer.optimize(objective_function, iters=max_iterations) # Optimize
 
     return best_position
 
-num_items = int(input("Enter number of items: "))
-max_weight = int(input("Enter maximum weight: "))
-items = np.empty((0, 2), int)
+num_items = int(input("Enter number of items: ")) # Number of items to choose from
+max_weight = int(input("Enter maximum weight: ")) # Maximum weight that the knapsack can hold
+items = np.empty((0, 2), int) # Declare empty array to store items
 
-for i in range(num_items):
-    value = int(input("Enter value of item " + str(i) + ": "))
-    weight = int(input("Enter weight of item " + str(i) + ": "))
-    items = np.append(items, [[value, weight]], axis=0)
+for i in range(num_items): # In range of number of items
+    value = int(input("Enter value of item " + str(i) + ": ")) # Ask user for value of item
+    weight = int(input("Enter weight of item " + str(i) + ": ")) # Ask user for weight of item
+    items = np.append(items, [[value, weight]], axis=0) # Append value and weight to items array
 
-best_solution = optimize_knapsack(num_items, max_weight, items, num_particles=20, max_iterations=100)
-print("Best solution:", best_solution)
+print("Best solution:", optimize_knapsack(num_items, max_weight, items, num_particles=20, max_iterations=100))
